aa2c78fd9cc4c378a725d62e953b75deeaa9b678
RyAMiller <ry.a.miller3@gmail.com>
2025-09-05 15:46:32 -0400

    update initial.R

--- spatialNAC/code/iSEE-app/initial.R
+++ spatialNAC/code/iSEE-app/initial.R
@@ -8,7 +8,7 @@ initial[["ReducedDimensionPlot1"]] <- new("ReducedDimensionPlot", Type = "tSNE_H
                                           YAxis = 2L, FacetRowByColData = "Sample", FacetColumnByColData = "Sample", 
                                           ColorByColumnData = "CellType.Final", ColorByFeatureNameAssay = "logcounts", 
                                           ColorBySampleNameColor = "#FF0000", ShapeByColumnData = "Sample", 
-                                          SizeByColumnData = "sum", TooltipColumnData = character(0), 
+                                          SizeByColumnData = NA_character_, TooltipColumnData = character(0), 
                                           FacetRowBy = "None", FacetColumnBy = "None", ColorBy = "Column data", 
                                           ColorByDefaultColor = "#000000", ColorByFeatureName = "KCNIP4", 
                                           ColorByFeatureSource = "---", ColorByFeatureDynamicSource = FALSE, 
@@ -22,8 +22,8 @@ initial[["ReducedDimensionPlot1"]] <- new("ReducedDimensionPlot", Type = "tSNE_H
                                           FontSize = 1, LegendPointSize = 1, LegendPosition = "Bottom", 
                                           HoverInfo = TRUE, LabelCenters = FALSE, LabelCentersBy = "Sample", 
                                           LabelCentersColor = "#000000", VersionInfo = list(iSEE = structure(list(
-                                            c(2L, 18L, 0L)), class = c("package_version", "numeric_version"
-                                            ))), PanelId = c(ReducedDimensionPlot = 1L), PanelHeight = 500L, 
+                                              c(2L, 20L, 0L)), class = c("package_version", "numeric_version"
+                                              ))), PanelId = c(ReducedDimensionPlot = 1L), PanelHeight = 500L, 
                                           PanelWidth = 4L, SelectionBoxOpen = FALSE, RowSelectionSource = "---", 
                                           ColumnSelectionSource = "---", DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, 
                                           ColumnSelectionDynamicSource = FALSE, RowSelectionRestrict = FALSE, 
@@ -40,7 +40,7 @@ initial[["FeatureAssayPlot1"]] <- new("FeatureAssayPlot", Assay = "logcounts", X
                                       FacetRowByColData = "Sample", FacetColumnByColData = "Sample", 
                                       ColorByColumnData = "CellType.Final", ColorByFeatureNameAssay = "logcounts", 
                                       ColorBySampleNameColor = "#FF0000", ShapeByColumnData = "Sample", 
-                                      SizeByColumnData = "sum", TooltipColumnData = character(0), 
+                                      SizeByColumnData = NA_character_, TooltipColumnData = character(0), 
                                       FacetRowBy = "None", FacetColumnBy = "None", ColorBy = "Column data", 
                                       ColorByDefaultColor = "#000000", ColorByFeatureName = "KCNIP4", 
                                       ColorByFeatureSource = "---", ColorByFeatureDynamicSource = FALSE, 
@@ -54,8 +54,8 @@ initial[["FeatureAssayPlot1"]] <- new("FeatureAssayPlot", Assay = "logcounts", X
                                       FontSize = 1, LegendPointSize = 1, LegendPosition = "Bottom", 
                                       HoverInfo = TRUE, LabelCenters = FALSE, LabelCentersBy = "Sample", 
                                       LabelCentersColor = "#000000", VersionInfo = list(iSEE = structure(list(
-                                        c(2L, 18L, 0L)), class = c("package_version", "numeric_version"
-                                        ))), PanelId = c(FeatureAssayPlot = 1L), PanelHeight = 500L, 
+                                          c(2L, 20L, 0L)), class = c("package_version", "numeric_version"
+                                          ))), PanelId = c(FeatureAssayPlot = 1L), PanelHeight = 500L, 
                                       PanelWidth = 4L, SelectionBoxOpen = FALSE, RowSelectionSource = "---", 
                                       ColumnSelectionSource = "---", DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, 
                                       ColumnSelectionDynamicSource = FALSE, RowSelectionRestrict = FALSE, 
@@ -67,15 +67,15 @@ initial[["FeatureAssayPlot1"]] <- new("FeatureAssayPlot", Assay = "logcounts", X
 
 initial[["ComplexHeatmapPlot1"]] <- new("ComplexHeatmapPlot", Assay = "logcounts", CustomRows = TRUE, 
                                         CustomRowsText = "GJA1\nDRD1\nDRD2\nCLDN5\nFOXJ1\nSLC17A7\nIL1RAPL2\nVIP\nGLP1R\nCHAT\nSST\nKCNC2\nARHGAP15\nST18\nPDGFRA", 
-                                        ClusterRows = FALSE, ClusterRowsDistance = "spearman", ClusterRowsMethod = "ward.D2", 
-                                        DataBoxOpen = FALSE, VisualChoices = "Annotations", ColumnData = "CellType.Final", 
-                                        RowData = character(0), CustomBounds = TRUE, LowerBound = -5L, 
-                                        UpperBound = 5L, AssayCenterRows = TRUE, AssayScaleRows = TRUE, 
-                                        DivergentColormap = "blue < white < red", ShowDimNames = "Rows", 
-                                        LegendPosition = "Bottom", LegendDirection = "Horizontal", 
+                                        CapRowSelection = 200L, ClusterRows = FALSE, ClusterRowsDistance = "spearman", 
+                                        ClusterRowsMethod = "ward.D2", DataBoxOpen = FALSE, VisualChoices = "Annotations", 
+                                        ColumnData = "CellType.Final", RowData = character(0), CustomBounds = TRUE, 
+                                        LowerBound = -5L, UpperBound = 5L, AssayCenterRows = TRUE, 
+                                        AssayScaleRows = TRUE, DivergentColormap = "blue < white < red", 
+                                        ShowDimNames = "Rows", LegendPosition = "Bottom", LegendDirection = "Horizontal", 
                                         VisualBoxOpen = FALSE, NamesRowFontSize = 10, NamesColumnFontSize = 10, 
                                         ShowColumnSelection = TRUE, OrderColumnSelection = TRUE, 
-                                        VersionInfo = list(iSEE = structure(list(c(2L, 18L, 0L)), class = c("package_version", 
+                                        VersionInfo = list(iSEE = structure(list(c(2L, 20L, 0L)), class = c("package_version", 
                                                                                                             "numeric_version"))), PanelId = c(ComplexHeatmapPlot = 1L), 
                                         PanelHeight = 500L, PanelWidth = 4L, SelectionBoxOpen = FALSE, 
                                         RowSelectionSource = "---", ColumnSelectionSource = "---", 
@@ -88,9 +88,9 @@ initial[["ComplexHeatmapPlot1"]] <- new("ComplexHeatmapPlot", Assay = "logcounts
 ################################################################################
 
 initial[["RowDataTable1"]] <- new("RowDataTable", Selected = "MIR1302-2HG", Search = "", SearchColumns = c("", 
-                                                                                                           "", "", "", "", "", "", "", "", "", ""), HiddenColumns = character(0), 
-                                  VersionInfo = list(iSEE = structure(list(c(2L, 18L, 0L)), class = c("package_version", 
-                                                                                                      "numeric_version"))), PanelId = 1L, PanelHeight = 500L, PanelWidth = 6L, 
+                                                                                                           "", "", "", "", ""), HiddenColumns = character(0), VersionInfo = list(
+                                                                                                               iSEE = structure(list(c(2L, 20L, 0L)), class = c("package_version", 
+                                                                                                                                                                "numeric_version"))), PanelId = 1L, PanelHeight = 500L, PanelWidth = 6L, 
                                   SelectionBoxOpen = FALSE, RowSelectionSource = "---", ColumnSelectionSource = "---", 
                                   DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, ColumnSelectionDynamicSource = FALSE, 
                                   RowSelectionRestrict = FALSE, ColumnSelectionRestrict = FALSE, 
@@ -101,9 +101,8 @@ initial[["RowDataTable1"]] <- new("RowDataTable", Selected = "MIR1302-2HG", Sear
 ################################################################################
 
 initial[["ColumnDataTable1"]] <- new("ColumnDataTable", Selected = "1_AAACCCAAGACCAACG-1", Search = "", 
-                                     SearchColumns = c("", "", "", "", "", "", "", "", "", "", 
-                                                       "", "", "", "", "", "", "", "", "", "", ""), HiddenColumns = character(0), 
-                                     VersionInfo = list(iSEE = structure(list(c(2L, 18L, 0L)), class = c("package_version", 
+                                     SearchColumns = c("", "", "", "", ""), HiddenColumns = character(0), 
+                                     VersionInfo = list(iSEE = structure(list(c(2L, 20L, 0L)), class = c("package_version", 
                                                                                                          "numeric_version"))), PanelId = 1L, PanelHeight = 500L, PanelWidth = 6L, 
                                      SelectionBoxOpen = FALSE, RowSelectionSource = "---", ColumnSelectionSource = "---", 
                                      DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, ColumnSelectionDynamicSource = FALSE, 
