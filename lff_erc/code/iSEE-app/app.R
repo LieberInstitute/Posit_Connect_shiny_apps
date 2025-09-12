c0605e48f2be44829a87db26b7a5e8393debd3dc
RyAMiller <ry.a.miller3@gmail.com>
2025-09-12 12:58:32 -0400

    updated app.R for lff-erc isee app

--- lff_erc/code/iSEE-app/app.R
+++ lff_erc/code/iSEE-app/app.R
@@ -11,32 +11,37 @@ options("golem.app.prod" = TRUE)
 ## You need this to enable shinyapps to install Bioconductor packages
 options(repos = BiocManager::repositories())
 
-## Load the data
-posit_connect_dir <- "/r_data/lcollado/Posit_Connect_shiny_apps/spatial-nac/processed-data/iSEE/sce_isee"
-repo_local_dir <- here::here(
-    "spatialNAC/processed-data/iSEE-app/sce_isee"
-)
 
-if (file.exists(posit_connect_dir)) {
+#sce <- readRDS("sce_ERC_iSEE.rds")
+#sn_colors <- readRDS("sn_colors.rds")
+
+
+## Load the data
+posit_connect_file <- "/r_data/lcollado/Posit_Connect_shiny_apps/lff_ERC/isee/sce_ERC_iSEE.rds"
+posit_connect_file1 <- "/r_data/lcollado/Posit_Connect_shiny_apps/lff_ERC/isee/sn_colors.rds"
+
+if (file.exists(posit_connect_file)) {
     ## Location for the https://conn1.libd.org/ server
-    dir_to_use <- posit_connect_dir
+    sce <- readRDS(posit_connect_file)
 } else {
-    dir_to_use <- repo_local_dir
+    sce <- readRDS(here::here("lff_erc/processed-data/iSEE-app/sce_ERC_iSEE.rds"))
 }
 
-sce <- loadHDF5SummarizedExperiment(
-    dir = dir_to_use
-)
+if (file.exists(posit_connect_file1)) {
+    ## Location for the https://conn1.libd.org/ server
+    sn_colors <- readRDS(posit_connect_file1)
+} else {
+    sn_colors <- readRDS(here::here("lff_erc/processed-data/iSEE-app/sn_colors.rds"))
+}
 
-#load("sce.Rdata", verbose = TRUE)
-#sce <- readRDS(file = "sce_NAc_app.Rds")
-#load("sce_for_iSEE_LS.rda", verbose = TRUE)
 
 #Change the rownames frome ensembl id to gene_name
 rownames(sce) <-  uniquifyFeatureNames(rowData(sce)$gene_id, rowData(sce)$gene_name)
 
+sce <- registerAppOptions(sce, color.maxlevels = 38)
+
 #Source
-source("https://github.com/LieberInstitute/Posit_Connect_shiny_apps/blob/devel/spatialNAC/code/iSEE-app/initial.R?raw=TRUE")
+source("https://github.com/LieberInstitute/Posit_Connect_shiny_apps/blob/devel/lff_erc/code/iSEE-app/initial.R?raw=TRUE")
 #source("initial.R", print.eval = TRUE)
 
 #Increase minimum number of colors to the # of clusters within CellType.Final
