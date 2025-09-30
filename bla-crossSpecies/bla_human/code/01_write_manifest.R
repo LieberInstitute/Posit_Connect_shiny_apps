d44fc2097fe46c6e9feeef0bee873c4b19b21b15
RyAMiller <ry.a.miller3@gmail.com>
2025-09-30 15:48:12 -0400

    updated app scripts for bla_human app

--- bla-crossSpecies/bla_human/code/01_write_manifest.R
+++ bla-crossSpecies/bla_human/code/01_write_manifest.R
@@ -4,7 +4,7 @@ library("here")
 library("withr")
 
 rsconnect::writeManifest(
-    appDir = here::here("bla-crossSpecies", "bla_baboon", "code"),
+    appDir = here::here("bla-crossSpecies", "bla_human", "code"),
     appFiles = c(
         "app.R",
         "initial.R"
--- bla-crossSpecies/bla_human/code/app.R
+++ bla-crossSpecies/bla_human/code/app.R
@@ -13,35 +13,33 @@ options("golem.app.prod" = TRUE)
 options(repos = BiocManager::repositories())
 
 ## Load the data
-posit_connect_file <- "/r_data/lcollado/Posit_Connect_shiny_apps/bla-crossspecies/bla-baboon/sce_FINAL_baboon.rda"
+posit_connect_file <- "/r_data/lcollado/Posit_Connect_shiny_apps/bla-crossspecies/bla-human/sce_FINAL_human.rda"
 
 if (file.exists(posit_connect_file)) {
     ## Location for the https://conn1.libd.org/ server
     load(posit_connect_file)
-    #sce <- rda.baboon
-    #rm(rda.baboon)
+
 } else {
-    load(here::here("bla-crossSpecies", "bla_baboon", "processed-data", "sce_FINAL_baboon.rda"))
-    #sce <- rda.baboon
-    #rm(rda.baboon)
+    load(here::here("bla-crossSpecies", "bla_human", "processed-data", "sce_FINAL_human.rda"))
+
 }
 
-rda.baboon$broad_celltype <- as.factor(rda.baboon$broad_celltype)
+rda.human$broad_celltype <- as.factor(rda.human$broad_celltype)
 
-source("https://github.com/LieberInstitute/Posit_Connect_shiny_apps/blob/devel/bla-crossSpecies/bla_baboon/code/initial.R?raw=TRUE")
+source("https://github.com/LieberInstitute/Posit_Connect_shiny_apps/blob/devel/bla-crossSpecies/bla_human/code/initial.R?raw=TRUE")
 
 
-mypalette = Polychrome::createPalette(length(unique(rda.baboon$fine_celltype)),  c("#ff0000", "#00ff00", "#0000ff"))
-colors <- setNames(mypalette, unique(rda.baboon$fine_celltype))
+mypalette = Polychrome::createPalette(length(unique(rda.human$fine_celltype)),  c("#ff0000", "#00ff00", "#0000ff"))
+colors <- setNames(mypalette, unique(rda.human$fine_celltype))
 
 
 #increase max number of colors
-rda.baboon <- registerAppOptions(rda.baboon, color.maxlevels = length(unique(rda.baboon$fine_celltype)))
+rda.human <- registerAppOptions(rda.human, color.maxlevels = length(unique(rda.human$fine_celltype)))
 
 #Deploy app
 iSEE(
-    rda.baboon,
-    appTitle = "BLA - Baboon",
+    rda.human,
+    appTitle = "BLA - Human",
     initial = initial,
     colormap = ExperimentColorMap(
         colData = list(fine_celltype = function(x) {
