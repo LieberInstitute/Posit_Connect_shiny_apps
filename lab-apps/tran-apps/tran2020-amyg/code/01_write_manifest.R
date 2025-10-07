f342b6d5c019e986e72cdfc9c305b46030a8891b
RyAMiller <ry.a.miller3@gmail.com>
2025-10-07 11:15:05 -0400

    updated tran2020-amyg app

--- lab-apps/tran-apps/tran2020-amyg/code/01_write_manifest.R
+++ lab-apps/tran-apps/tran2020-amyg/code/01_write_manifest.R
@@ -6,6 +6,7 @@ library("withr")
 rsconnect::writeManifest(
     appDir = here::here("lab-apps", "tran-apps", "tran2020-amyg", "code"),
     appFiles = c(
-        "app.R"
+        "app.R",
+        "initial.R"
     )
 )
