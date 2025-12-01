02f0ff6072f40b4228c449e208f50ea65adf338b
RyAMiller <ry.a.miller3@gmail.com>
2025-12-01 10:55:05 -0500

    dACC - snRNAseq initial.R file

---
+++ dACC/snRNAseq_dACC/code/initial.R
@@ -0,0 +1,85 @@
+initial <- list()
+
+################################################################################
+# Settings for Reduced dimension plot 1
+################################################################################
+
+initial[["ReducedDimensionPlot1"]] <- new("ReducedDimensionPlot", Type = "UMAP-HARMONY", XAxis = 1L,
+                                          YAxis = 2L, FacetRowByColData = "sort", FacetColumnByColData = "sort",
+                                          ColorByColumnData = "cellType_azimuth", ColorByFeatureNameAssay = "logcounts",
+                                          ColorBySampleNameColor = "#FF0000", ShapeByColumnData = "sort",
+                                          SizeByColumnData = "sum", TooltipColumnData = character(0),
+                                          FacetRowBy = "None", FacetColumnBy = "None", ColorBy = "Column data",
+                                          ColorByDefaultColor = "#000000", ColorByFeatureName = "KCNIP4",
+                                          ColorByFeatureSource = "---", ColorByFeatureDynamicSource = FALSE,
+                                          ColorBySampleName = "1_AAACCCAAGATGAAGG-1", ColorBySampleSource = "---",
+                                          ColorBySampleDynamicSource = FALSE, ShapeBy = "None", SizeBy = "None",
+                                          SelectionAlpha = 0.1, ZoomData = numeric(0), BrushData = list(),
+                                          VisualBoxOpen = FALSE, VisualChoices = "Color", ContourAdd = FALSE,
+                                          ContourColor = "#0000FF", PointSize = 1, PointAlpha = 1,
+                                          Downsample = FALSE, DownsampleResolution = 200, CustomLabels = FALSE,
+                                          CustomLabelsText = "1_AAACCCAAGATGAAGG-1", FontSize = 1,
+                                          LegendPointSize = 1, LegendPosition = "Bottom", HoverInfo = TRUE,
+                                          LabelCenters = FALSE, LabelCentersBy = "sort", LabelCentersColor = "#000000",
+                                          VersionInfo = list(iSEE = structure(list(c(2L, 17L, 3L)), class = c("package_version",
+                                                                                                              "numeric_version"))), PanelId = c(ReducedDimensionPlot = 1L),
+                                          PanelHeight = 500L, PanelWidth = 4L, SelectionBoxOpen = FALSE,
+                                          RowSelectionSource = "---", ColumnSelectionSource = "---",
+                                          DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, ColumnSelectionDynamicSource = FALSE,
+                                          RowSelectionRestrict = FALSE, ColumnSelectionRestrict = FALSE,
+                                          SelectionHistory = list())
+
+################################################################################
+# Settings for Feature assay plot 1
+################################################################################
+
+initial[["FeatureAssayPlot1"]] <- new("FeatureAssayPlot", Assay = "logcounts", XAxis = "Column data",
+                                      XAxisColumnData = "cellType_azimuth", XAxisFeatureName = "ADRA1A",
+                                      XAxisFeatureSource = "---", XAxisFeatureDynamicSource = FALSE,
+                                      YAxisFeatureName = "ADRA1A", YAxisFeatureSource = "---",
+                                      YAxisFeatureDynamicSource = FALSE, FacetRowByColData = "sort",
+                                      FacetColumnByColData = "sort", ColorByColumnData = "cellType_azimuth",
+                                      ColorByFeatureNameAssay = "logcounts", ColorBySampleNameColor = "#FF0000",
+                                      ShapeByColumnData = "sort", SizeByColumnData = "sum", TooltipColumnData = character(0),
+                                      FacetRowBy = "None", FacetColumnBy = "None", ColorBy = "Column data",
+                                      ColorByDefaultColor = "#000000", ColorByFeatureName = "KCNIP4",
+                                      ColorByFeatureSource = "---", ColorByFeatureDynamicSource = FALSE,
+                                      ColorBySampleName = "1_AAACCCAAGATGAAGG-1", ColorBySampleSource = "---",
+                                      ColorBySampleDynamicSource = FALSE, ShapeBy = "None", SizeBy = "None",
+                                      SelectionAlpha = 0.1, ZoomData = numeric(0), BrushData = list(),
+                                      VisualBoxOpen = FALSE, VisualChoices = "Color", ContourAdd = FALSE,
+                                      ContourColor = "#0000FF", PointSize = 1, PointAlpha = 1,
+                                      Downsample = FALSE, DownsampleResolution = 200, CustomLabels = FALSE,
+                                      CustomLabelsText = "1_AAACCCAAGATGAAGG-1", FontSize = 1,
+                                      LegendPointSize = 1, LegendPosition = "Bottom", HoverInfo = TRUE,
+                                      LabelCenters = FALSE, LabelCentersBy = "sort", LabelCentersColor = "#000000",
+                                      VersionInfo = list(iSEE = structure(list(c(2L, 17L, 3L)), class = c("package_version",
+                                                                                                          "numeric_version"))), PanelId = c(FeatureAssayPlot = 1L),
+                                      PanelHeight = 500L, PanelWidth = 4L, SelectionBoxOpen = FALSE,
+                                      RowSelectionSource = "---", ColumnSelectionSource = "---",
+                                      DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, ColumnSelectionDynamicSource = FALSE,
+                                      RowSelectionRestrict = FALSE, ColumnSelectionRestrict = FALSE,
+                                      SelectionHistory = list())
+
+################################################################################
+# Settings for Complex heatmap 1
+################################################################################
+
+initial[["ComplexHeatmapPlot1"]] <- new("ComplexHeatmapPlot", Assay = "logcounts", CustomRows = TRUE,
+                                        CustomRowsText = c("CUX2", "ADCYAP1", "VAT1L", "GABRQ", "ROR1B", "CASC15", "PCP4",
+                                                           "HTR2C", "SGCG", "SEMA5A", "GPR6", "CCN4", "CUX1", "SEMA6D","ZFHX3", "NXPH4"),
+                                        ClusterRows = FALSE, ClusterRowsDistance = "spearman",
+                                        ClusterRowsMethod = "ward.D2", DataBoxOpen = FALSE, VisualChoices = "Annotations",
+                                        ColumnData = "cellType_azimuth", RowData = character(0), CustomBounds = FALSE,
+                                        LowerBound = NA_real_, UpperBound = NA_real_, AssayCenterRows = FALSE,
+                                        AssayScaleRows = FALSE, DivergentColormap = "blue < white < red",
+                                        ShowDimNames = "Rows", LegendPosition = "Bottom", LegendDirection = "Horizontal",
+                                        VisualBoxOpen = FALSE, NamesRowFontSize = 10, NamesColumnFontSize = 10,
+                                        ShowColumnSelection = TRUE, OrderColumnSelection = TRUE,
+                                        VersionInfo = list(iSEE = structure(list(c(2L, 17L, 3L)), class = c("package_version",
+                                                                                                            "numeric_version"))), PanelId = c(ComplexHeatmapPlot = 1L),
+                                        PanelHeight = 500L, PanelWidth = 4L, SelectionBoxOpen = FALSE,
+                                        RowSelectionSource = "---", ColumnSelectionSource = "---",
+                                        RowSelectionDynamicSource = FALSE, ColumnSelectionDynamicSource = FALSE,
+                                        RowSelectionRestrict = FALSE, ColumnSelectionRestrict = FALSE,
+                                        SelectionHistory = list())
