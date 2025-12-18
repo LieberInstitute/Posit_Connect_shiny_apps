34eaf67184a9620f937c54d2050a3017e1b3040a
RyAMiller <ry.a.miller3@gmail.com>
2025-12-18 15:29:51 -0500

    app files for spatialDLPFC_Visium_Sp09_pseudobulk

---
+++ dlpfc/spatialDLPFC_Visium_Sp09_pseudobulk/code/01_write_manifest.R
@@ -0,0 +1,11 @@
+library("rsconnect")
+library("here")
+library("withr")
+
+rsconnect::writeManifest(
+    appDir = here::here("dlpfc", "spatialDLPFC_Visium_Sp09_pseudobulk", "code"),
+    appFiles = c(
+        "app.R",
+        "initial.R"
+    )
+)
