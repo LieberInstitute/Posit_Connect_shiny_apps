ac31de62c023a920b9b41b5a4e2b86a29d3c45c3
RyAMiller <ry.a.miller3@gmail.com>
2026-07-09 13:43:46 -0400

    updated initial.R file for lff lc project

--- lff_lc/code/iSEE/initial.R
+++ lff_lc/code/iSEE/initial.R
@@ -1,81 +1,114 @@
 initial <- list()
 
 ################################################################################
+# Settings for Column data table 1
+################################################################################
+
+initial[["SubSetDataTable"]] <- new("ColumnDataTable", Selected = "ACHE_SERT_LC_WM_V13B23_284_A1s1",
+                                     Search = "", SearchColumns = c("", "", "", "", "", "", "",
+                                                                    "", "", "LC$", ""), HiddenColumns = character(0), VersionInfo = list(
+                                                                        iSEE = structure(list(c(2L, 20L, 0L)), class = c("package_version",
+                                                                                                                         "numeric_version"))), PanelId = 1L, PanelHeight = 500L,
+                                     PanelWidth = 12L, SelectionBoxOpen = FALSE, RowSelectionSource = "---",
+                                     ColumnSelectionSource = "---", DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE,
+                                     ColumnSelectionDynamicSource = FALSE, RowSelectionRestrict = FALSE,
+                                     ColumnSelectionRestrict = FALSE, SelectionHistory = list())
+
+################################################################################
 # Settings for Feature assay plot 1
 ################################################################################
 
-initial[["FeatureAssayPlot1"]] <- new("FeatureAssayPlot", Assay = "counts", XAxis = "Column data", 
-                                      XAxisColumnData = "APOE Carrier Type", XAxisFeatureName = "NDRG2", 
-                                      XAxisFeatureSource = "---", XAxisFeatureDynamicSource = FALSE, 
-                                      YAxisFeatureName = "HMGCS1", YAxisFeatureSource = "---", 
-                                      YAxisFeatureDynamicSource = FALSE, FacetRowByColData = "Ancestry", 
-                                      FacetColumnByColData = "sample_id", ColorByColumnData = "Sex", 
-                                      ColorByFeatureNameAssay = "counts", ColorBySampleNameColor = "#FF0000", 
-                                      ShapeByColumnData = "Sex", SizeByColumnData = "Age", TooltipColumnData = character(0), 
-                                      FacetRowBy = "Column data", FacetColumnBy = "None", ColorBy = "Column data", 
-                                      ColorByDefaultColor = "#000000", ColorByFeatureName = "NDRG2", 
-                                      ColorByFeatureSource = "---", ColorByFeatureDynamicSource = FALSE, 
-                                      ColorBySampleName = "ACHE_SERT_LC_WM_V13B23_284_A1s1", ColorBySampleSource = "---", 
-                                      ColorBySampleDynamicSource = FALSE, ShapeBy = "Column data", 
-                                      SizeBy = "None", SelectionAlpha = 0.1, ZoomData = numeric(0), 
-                                      BrushData = list(), VisualBoxOpen = FALSE, VisualChoices = c("Color", 
-                                                                                                   "Shape", "Size", "Facet"), ContourAdd = FALSE, ContourColor = "#0000FF", 
-                                      FixAspectRatio = FALSE, ViolinAdd = TRUE, PointSize = 2, 
-                                      PointAlpha = 1, Downsample = FALSE, DownsampleResolution = 200, 
-                                      CustomLabels = FALSE, CustomLabelsText = "ACHE_SERT_LC_WM_V13B23_284_A1s1", 
-                                      FontSize = 1, LegendPointSize = 1, LegendPosition = "Bottom", 
-                                      HoverInfo = TRUE, LabelCenters = FALSE, LabelCentersBy = "sample_id", 
+initial[["FeatureAssayPlot1"]] <- new("FeatureAssayPlot", Assay = "logcpm", XAxis = "Column data",
+                                      XAxisColumnData = "APOE Carrier Type", XAxisFeatureName = "NDRG2",
+                                      XAxisFeatureSource = "---", XAxisFeatureDynamicSource = FALSE,
+                                      YAxisFeatureName = "HMGCS1", YAxisFeatureSource = "---",
+                                      YAxisFeatureDynamicSource = FALSE, FacetRowByColData = "Ancestry",
+                                      FacetColumnByColData = "sample_id", ColorByColumnData = "Sex",
+                                      ColorByFeatureNameAssay = "counts", ColorBySampleNameColor = "#FF0000",
+                                      ShapeByColumnData = "Sex", SizeByColumnData = "Age", TooltipColumnData = character(0),
+                                      FacetRowBy = "Column data", FacetColumnBy = "None", ColorBy = "Column data",
+                                      ColorByDefaultColor = "#000000", ColorByFeatureName = "NDRG2",
+                                      ColorByFeatureSource = "---", ColorByFeatureDynamicSource = FALSE,
+                                      ColorBySampleName = "ACHE_SERT_LC_WM_V13B23_284_A1s1", ColorBySampleSource = "---",
+                                      ColorBySampleDynamicSource = FALSE, ShapeBy = "Column data",
+                                      SizeBy = "None", SelectionAlpha = 0.1, ZoomData = numeric(0),
+                                      BrushData = list(), VisualBoxOpen = FALSE, VisualChoices = c("Color",
+                                                                                                   "Shape", "Size", "Facet"), ContourAdd = FALSE, ContourColor = "#0000FF",
+                                      FixAspectRatio = FALSE, ViolinAdd = TRUE, PointSize = 2,
+                                      PointAlpha = 1, Downsample = FALSE, DownsampleResolution = 200,
+                                      CustomLabels = FALSE, CustomLabelsText = "ACHE_SERT_LC_WM_V13B23_284_A1s1",
+                                      FontSize = 1, LegendPointSize = 1, LegendPosition = "Bottom",
+                                      HoverInfo = TRUE, LabelCenters = FALSE, LabelCentersBy = "sample_id",
                                       LabelCentersColor = "#000000", VersionInfo = list(iSEE = structure(list(
                                           c(2L, 20L, 0L)), class = c("package_version", "numeric_version"
-                                          ))), PanelId = c(FeatureAssayPlot = 1L), PanelHeight = 550L, 
-                                      PanelWidth = 12L, SelectionBoxOpen = FALSE, RowSelectionSource = "---", 
-                                      ColumnSelectionSource = "---", DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, 
-                                      ColumnSelectionDynamicSource = FALSE, RowSelectionRestrict = FALSE, 
+                                          ))), PanelId = c(FeatureAssayPlot = 1L), PanelHeight = 550L,
+                                      PanelWidth = 12L, SelectionBoxOpen = FALSE, RowSelectionSource = "---",
+                                      ColumnSelectionSource = "---", DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE,
+                                      ColumnSelectionDynamicSource = FALSE, RowSelectionRestrict = FALSE,
                                       ColumnSelectionRestrict = FALSE, SelectionHistory = list())
 
 ################################################################################
 # Settings for Feature assay plot 2
 ################################################################################
 
-initial[["FeatureAssayPlot2"]] <- new("FeatureAssayPlot", Assay = "counts", XAxis = "Column data", 
-                                      XAxisColumnData = "Domain", XAxisFeatureName = "NDRG2", XAxisFeatureSource = "---", 
-                                      XAxisFeatureDynamicSource = FALSE, YAxisFeatureName = "HMGCS1", 
-                                      YAxisFeatureSource = "---", YAxisFeatureDynamicSource = FALSE, 
-                                      FacetRowByColData = "Ancestry", FacetColumnByColData = "Ancestry", 
-                                      ColorByColumnData = "Sex", ColorByFeatureNameAssay = "counts", 
-                                      ColorBySampleNameColor = "#FF0000", ShapeByColumnData = "Sex", 
-                                      SizeByColumnData = "Age", TooltipColumnData = character(0), 
-                                      FacetRowBy = "None", FacetColumnBy = "Column data", ColorBy = "Column data", 
-                                      ColorByDefaultColor = "#000000", ColorByFeatureName = "NDRG2", 
-                                      ColorByFeatureSource = "---", ColorByFeatureDynamicSource = FALSE, 
-                                      ColorBySampleName = "ACHE_SERT_LC_WM_V13B23_284_A1s1", ColorBySampleSource = "---", 
-                                      ColorBySampleDynamicSource = FALSE, ShapeBy = "Column data", 
-                                      SizeBy = "None", SelectionAlpha = 0.1, ZoomData = numeric(0), 
-                                      BrushData = list(), VisualBoxOpen = FALSE, VisualChoices = c("Color", 
-                                                                                                   "Shape", "Size", "Facet"), ContourAdd = FALSE, ContourColor = "#0000FF", 
-                                      FixAspectRatio = FALSE, ViolinAdd = TRUE, PointSize = 2, 
-                                      PointAlpha = 1, Downsample = FALSE, DownsampleResolution = 200, 
-                                      CustomLabels = FALSE, CustomLabelsText = "ACHE_SERT_LC_WM_V13B23_284_A1s1", 
-                                      FontSize = 1, LegendPointSize = 1, LegendPosition = "Bottom", 
-                                      HoverInfo = TRUE, LabelCenters = FALSE, LabelCentersBy = "sample_id", 
+initial[["FeatureAssayPlot2"]] <- new("FeatureAssayPlot", Assay = "logcpm", XAxis = "Column data",
+                                      XAxisColumnData = "Domain", XAxisFeatureName = "NDRG2", XAxisFeatureSource = "---",
+                                      XAxisFeatureDynamicSource = FALSE, YAxisFeatureName = "HMGCS1",
+                                      YAxisFeatureSource = "---", YAxisFeatureDynamicSource = FALSE,
+                                      FacetRowByColData = "Ancestry", FacetColumnByColData = "Ancestry",
+                                      ColorByColumnData = "Sex", ColorByFeatureNameAssay = "counts",
+                                      ColorBySampleNameColor = "#FF0000", ShapeByColumnData = "Sex",
+                                      SizeByColumnData = "Age", TooltipColumnData = character(0),
+                                      FacetRowBy = "None", FacetColumnBy = "Column data", ColorBy = "Column data",
+                                      ColorByDefaultColor = "#000000", ColorByFeatureName = "NDRG2",
+                                      ColorByFeatureSource = "---", ColorByFeatureDynamicSource = FALSE,
+                                      ColorBySampleName = "ACHE_SERT_LC_WM_V13B23_284_A1s1", ColorBySampleSource = "---",
+                                      ColorBySampleDynamicSource = FALSE, ShapeBy = "Column data",
+                                      SizeBy = "None", SelectionAlpha = 0.1, ZoomData = numeric(0),
+                                      BrushData = list(), VisualBoxOpen = FALSE, VisualChoices = c("Color",
+                                                                                                   "Shape", "Size", "Facet"), ContourAdd = FALSE, ContourColor = "#0000FF",
+                                      FixAspectRatio = FALSE, ViolinAdd = TRUE, PointSize = 2,
+                                      PointAlpha = 1, Downsample = FALSE, DownsampleResolution = 200,
+                                      CustomLabels = FALSE, CustomLabelsText = "ACHE_SERT_LC_WM_V13B23_284_A1s1",
+                                      FontSize = 1, LegendPointSize = 1, LegendPosition = "Bottom",
+                                      HoverInfo = TRUE, LabelCenters = FALSE, LabelCentersBy = "sample_id",
                                       LabelCentersColor = "#000000", VersionInfo = list(iSEE = structure(list(
                                           c(2L, 20L, 0L)), class = c("package_version", "numeric_version"
-                                          ))), PanelId = 2L, PanelHeight = 550L, PanelWidth = 12L, 
-                                      SelectionBoxOpen = FALSE, RowSelectionSource = "---", ColumnSelectionSource = "ColumnDataTable1", 
-                                      DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, ColumnSelectionDynamicSource = TRUE, 
-                                      RowSelectionRestrict = FALSE, ColumnSelectionRestrict = TRUE, 
+                                          ))), PanelId = 2L, PanelHeight = 550L, PanelWidth = 12L,
+                                      SelectionBoxOpen = FALSE, RowSelectionSource = "---", ColumnSelectionSource = "SubSetDataTable",
+                                      DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, ColumnSelectionDynamicSource = TRUE,
+                                      RowSelectionRestrict = FALSE, ColumnSelectionRestrict = TRUE,
                                       SelectionHistory = list())
 
 ################################################################################
-# Settings for Column data table 1
+# Settings for Feature assay plot 3
 ################################################################################
 
-initial[["ColumnDataTable1"]] <- new("ColumnDataTable", Selected = "ACHE_SERT_LC_WM_V13B23_284_A1s1", 
-                                     Search = "", SearchColumns = c("", "", "", "", "", "", "", 
-                                                                    "", "", "LC", ""), HiddenColumns = character(0), VersionInfo = list(
-                                                                        iSEE = structure(list(c(2L, 20L, 0L)), class = c("package_version", 
-                                                                                                                         "numeric_version"))), PanelId = 1L, PanelHeight = 500L, 
-                                     PanelWidth = 4L, SelectionBoxOpen = FALSE, RowSelectionSource = "---", 
-                                     ColumnSelectionSource = "---", DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, 
-                                     ColumnSelectionDynamicSource = FALSE, RowSelectionRestrict = FALSE, 
-                                     ColumnSelectionRestrict = FALSE, SelectionHistory = list())
+initial[["FeatureAssayPlot3"]] <- new("FeatureAssayPlot", Assay = "logcpm", XAxis = "Column data",
+                                      XAxisColumnData = "Domain", XAxisFeatureName = "NDRG2", XAxisFeatureSource = "---",
+                                      XAxisFeatureDynamicSource = FALSE, YAxisFeatureName = "HMGCS1",
+                                      YAxisFeatureSource = "---", YAxisFeatureDynamicSource = FALSE,
+                                      FacetRowByColData = "sample_id", FacetColumnByColData = "Ancestry",
+                                      ColorByColumnData = "Sex", ColorByFeatureNameAssay = "counts",
+                                      ColorBySampleNameColor = "#FF0000", ShapeByColumnData = "Sex",
+                                      SizeByColumnData = "Age", TooltipColumnData = character(0),
+                                      FacetRowBy = "None", FacetColumnBy = "Column data", ColorBy = "Column data",
+                                      ColorByDefaultColor = "#000000", ColorByFeatureName = "NDRG2",
+                                      ColorByFeatureSource = "---", ColorByFeatureDynamicSource = FALSE,
+                                      ColorBySampleName = "ACHE_SERT_LC_WM_V13B23_284_A1s1", ColorBySampleSource = "---",
+                                      ColorBySampleDynamicSource = FALSE, ShapeBy = "Column data",
+                                      SizeBy = "None", SelectionAlpha = 0.1, ZoomData = numeric(0),
+                                      BrushData = list(), VisualBoxOpen = FALSE, VisualChoices = c("Color",
+                                                                                                   "Shape", "Size", "Facet"), ContourAdd = FALSE, ContourColor = "#0000FF",
+                                      FixAspectRatio = FALSE, ViolinAdd = TRUE, PointSize = 2,
+                                      PointAlpha = 1, Downsample = FALSE, DownsampleResolution = 200,
+                                      CustomLabels = FALSE, CustomLabelsText = "ACHE_SERT_LC_WM_V13B23_284_A1s1",
+                                      FontSize = 1, LegendPointSize = 1, LegendPosition = "Bottom",
+                                      HoverInfo = TRUE, LabelCenters = FALSE, LabelCentersBy = "sample_id",
+                                      LabelCentersColor = "#000000", VersionInfo = list(iSEE = structure(list(
+                                          c(2L, 20L, 0L)), class = c("package_version", "numeric_version"
+                                          ))), PanelId = 3L, PanelHeight = 500L, PanelWidth = 12L,
+                                      SelectionBoxOpen = FALSE, RowSelectionSource = "---", ColumnSelectionSource = "---",
+                                      DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, ColumnSelectionDynamicSource = FALSE,
+                                      RowSelectionRestrict = FALSE, ColumnSelectionRestrict = FALSE,
+                                      SelectionHistory = list())
