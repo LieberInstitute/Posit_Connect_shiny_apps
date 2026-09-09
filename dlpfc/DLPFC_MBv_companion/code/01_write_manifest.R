bfabec145d2f8042d0e42bd295de633aeba1e505
RyAMiller <ry.a.miller3@gmail.com>
2026-09-09 15:33:57 -0400

    updated app file and manifest for dlpfc mbv companion app

--- dlpfc/DLPFC_MBv_companion/code/01_write_manifest.R
+++ dlpfc/DLPFC_MBv_companion/code/01_write_manifest.R
@@ -5,8 +5,7 @@ library("withr")
 rsconnect::writeManifest(
     appDir = here::here("dlpfc", "DLPFC_MBv_companion", "code"),
     appFiles = c(
-        "ui.R",
-        "server.R",
+        "app.R",
         withr::with_dir(
             here::here("dlpfc", "DLPFC_MBv_companion", "code"),
             dir("www", full.names = TRUE)
